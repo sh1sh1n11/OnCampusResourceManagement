bd00af800300c8c4f9066f1104fd6c0806447840
sh1sh1n11 <shankynimje@gmail.com>
2015-03-21 01:05:27 -0700

    second comment

--- app/src/main/java/msisproject1/com/project1/MainActivity4.java
+++ app/src/main/java/msisproject1/com/project1/MainActivity4.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+//this is second comment
 public class MainActivity4 extends ActionBarActivity {
 
     @Override
